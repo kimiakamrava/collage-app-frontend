ba2998fce7087a542491d4b8c7d4139c147931a0
kimiakamrava <kimiakamrava93@gmail.com>
2021-02-28 12:59:23 -0800

    some routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { createPalette } from "./materialHelpers";
 
 class App extends Component {
   render() {
-    console.log(createPalette(seedColors[4]))
     return(
-      <div>
-        <Palette palette={createPalette(seedColors[4])} />
-      </div>
-    )
+      <Switch>
+        <Route exact path="/" render={() =><h1>sign in goes here</h1>}/>
+        <Route exact path="/palette/:id" render={() =><h1>single palette</h1>}/>
+        <Route exact path="/palettes" render={() =><h1>my palette</h1>}/>
+        
+      </Switch>
+      
+      // <div>
+      //   <Palette palette={createPalette(seedColors[4])} />
+      // </div>
+    );
   }
 }
 
