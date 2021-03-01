ff6a24d0a749965309b7d549cf843b1850ce5ce0
kimiakamrava <kimiakamrava93@gmail.com>
2021-02-28 19:53:52 -0800

    show the palette in homepage

--- src/HomePalette.js
+++ src/HomePalette.js
@@ -4,7 +4,7 @@ import { withStyles } from '@material-ui/styles';
 const style = {
     root:{
       backgroundColor: "white",
-      border: "1px solid black",
+      border: "2px solid gray",
       borderRadius:"5px",
       padding:"0.5rem",
       position: "relative",
@@ -14,7 +14,11 @@ const style = {
         }
     },
     colors: {
-      backgroundColor: "grey",
+      backgroundColor: "white",
+      height:"150px",
+      width: "100%",
+      borderRadius: "5px" ,
+      overFlow: "hidden",
     },
     images: {
       display:"flex"
@@ -24,21 +28,39 @@ const style = {
       justifyContent: "space-between",
       alignItems: "center",
       margin: "0",
-      color: "black",
+      color: "gray",
       paddingTop: "0.5rem",
       fontSize: "1rem",
       position: "relative",
     },
+    lilColor:{
+        height:"50%",
+        width:"10%",
+        display: "inline-block",
+        margin:"0 auto",
+        position: "relative",
+        marginBottom:"-2px",
+        borderRadius:"hidden",
+    }
 };
 
 function HomePalette(props) {
-    const {classes, paletteName} = props;
+    const {classes, paletteName, colors} = props;
+    const lilColorGrid = colors.map(color => (
+        <div className={classes.lilColor}
+         style={{backgroundColor: color.color}}
+         key={color.name}
+        />
+    ))
     return (
         <div className={classes.root}>
-        <div className={classes.colors}></div>    
-        <h5 className={classes.title}>{paletteName}</h5>
+        <div className={classes.colors}>
+        {lilColorGrid}
+        </div>    
+        <h5 className={classes.title}>{paletteName}
+        </h5>
         </div>
-    )
+    );
 }
 
 export default withStyles(style)(HomePalette);
--- src/PaletteAll.js
+++ src/PaletteAll.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
 import HomePalette from './HomePalette';
 import { Link } from "react-router-dom";
+import { withStyles } from '@material-ui/styles';
+
+const style = {
+    root: {
+        backgroundColor: "white",
+        height: "100vh",
+        display: "flex",
+        alignItems:"flex-start",
+        justifyContent: "center",
+        
+    },
+    container: {
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection : "column",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        fontSize: "8px"
+        
+    },
+    nav: {
+        display: "flex",
+        alignItems: "center",
+        height: "6vh",
+        fontSize: "7px",
+        width : "100%",
+        justifyContent: "space-between",
+        color: "gray",   
+    },
+    palettes:{
+      boxSizing: "border-box",
+      width: "100%",
+      display: "grid",
+      gridTemplateColumns:"repeat(2, 45%)",
+      gridGap: "2%",
+      fontSize: "8px",
+    }
+};
 
 class PaletteAll extends Component {
     render() {
-        const { palettes } = this.props;
+        const { palettes, classes } = this.props;
         return (
-            <div>
-                <HomePalette/>
-                <h1>My Palettes</h1>
-                {palettes.map(palette => (
-                 <HomePalette {...palette}/>
-                ))}    
-
+            <div className={classes.root}>
+                <div className={classes.container}>
+                    <nav className={classes.nav}>
+                     <h1>My Palettes</h1>
+                    </nav>
+                   <div className={classes.palettes}>  
+                     {palettes.map(palette => (
+                       <HomePalette {...palette}/>
+                    ))}  
+                    </div> 
+                </div>
             </div>
         );
     }
 }
 
-export default PaletteAll;
+export default withStyles(style)(PaletteAll);
